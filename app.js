d85f5205fe9b3410f5a0680bead0a1939bc8f84c
Dmitriy <Chuhlomin.d@mail.ru>
2021-02-03 15:12:59 +0300

    edit info

--- app.js
+++ app.js
@@ -28,6 +28,39 @@ if (notes.length < 1) response.redirect("/");
 response.render("info", notes[0]);
 });
 
+app.get("/admin/edit/:id", (request, response) => {
+const id = request.params.id;
+const file = fs.readFileSync("./data/notes.json");
+const model = JSON.parse(file);
+const notes = model.notes.filter(x => x.id == id);
+
+let note = {};
+if (notes.length > 0) note = notes[0];
+
+response.render("info", note);
+});
+
+app.post("/admin/edit", (request, response) => {
+const editInfo = request.body;
+if (!editInfo) response.redirect("/");
+
+const file = fs.readFileSync("./data/notes.json");
+const model = JSON.parse(file);
+const notes = model.notes.filter(x => x.id == id);
+
+if (notes.length < 1) {
+const note = notes[0];
+
+note.title = editInfo.title;
+note.text = editInfo.text;
+} else {
+note.id = notes[notes.length - 1].id + 1;
+notes.push(note);
+}
+
+response.redirect("/");
+});
+
 app.get("/*", (request, response) => {
 response.redirect("/");
 });
