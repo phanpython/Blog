42be9562190b13edd171a6cac4c1a2118ab1c776
Dmitriy <Chuhlomin.d@mail.ru>
2021-02-19 00:22:56 +0300

    add dark  theme

--- app.js
+++ app.js
@@ -5,111 +5,114 @@ const bodyParser = require('body-parser');
 const app = express();
 const port = process.env.PORT || 2000;
 const auth = {
-login: "admin",
-pass: "1234"
+    login: "admin",
+    pass: "1234"
 }
 
 app.set("view engine", "hbs");
 
+app.use(express.static(__dirname + '/views'));
+
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use("/admin",(request, response, next) => {
-const user = request.query;
+app.use("/admin", (request, response, next) => {
+    const user = request.query;
 
-if (!user || user.login != auth.login || user.pass != auth.pass) {
-    response.header({"Content-Type": "text/html; charset=utf-8"});
-    response.status(401);
-    response.send("Ошибка аторизации");
-}
+    if (!user || user.login != auth.login || user.pass != auth.pass) {
+        response.header({ "Content-Type": "text/html; charset=utf-8" });
+        response.status(401);
+        response.send("Ошибка аторизации");
+    }
     next();
 });
 
 app.listen(port, () => {
-console.log(`App is running on http://localhost:${port}/`);
+    console.log(`App is running on http://localhost:${port}/`);
 });
 
 app.get("/", (request, response) => {
-const file = fs.readFileSync("./data/notes.json");
+    const file = fs.readFileSync("./data/notes.json");
 
-const model = JSON.parse(file);
-response.render("index", model);
+    const model = JSON.parse(file);
+    response.render("index", model);
 });
 
 app.get("/info/:id", (request, response) => {
-const id = request.params.id;
-const file = fs.readFileSync("./data/notes.json");
-const model = JSON.parse(file);
-const notes = model.notes.filter(x => x.id == id);
+    const id = request.params.id;
+    const file = fs.readFileSync("./data/notes.json");
+    const model = JSON.parse(file);
+    const notes = model.notes.filter(x => x.id == id);
 
-if (notes.length < 1) response.redirect("/");
+    if (notes.length < 1) response.redirect("/");
 
-response.render("info", notes[0]);
+    response.render("info", notes[0]);
 });
 
 app.get("/admin/edit/:id", (request, response) => {
-const id = request.params.id;
+    const id = request.params.id;
 
-const file = fs.readFileSync("./data/notes.json");
-const model = JSON.parse(file);
-const notes = model.notes.filter(x => x.id == id);
+    const file = fs.readFileSync("./data/notes.json");
+    const model = JSON.parse(file);
+    const notes = model.notes.filter(x => x.id == id);
 
-let note = {};
-if (notes.length > 0) note = notes[0];
+    let note = {};
+    if (notes.length > 0) note = notes[0];
 
-response.render("edit-info", note);
+    response.render("edit-info", note);
 });
 
 app.get("/admin/add", (request, response) => {
-const note = {};
+    const note = {};
 
-response.render("edit-info", note);
+    response.render("edit-info", note);
 });
 
 app.post("/admin/edit", (request, response) => {
-const editInfo = request.body;
-if (!editInfo) response.redirect("/");
+    const editInfo = request.body;
+    if (!editInfo) response.redirect("/");
 
-editInfo.id = Number(editInfo.id);
+    editInfo.id = Number(editInfo.id);
 
-const file = fs.readFileSync("./data/notes.json");
-const model = JSON.parse(file);
-const notes = model.notes.filter(x => x.id == editInfo.id);
-let note = {};
-if (notes.length > 0) {
-note = notes[0];
+    const file = fs.readFileSync("./data/notes.json");
+    const model = JSON.parse(file);
+    const notes = model.notes.filter(x => x.id == editInfo.id);
+    let note = {};
+    if (notes.length > 0) {
+        note = notes[0];
+        note.image = editInfo.image;
+        note.title = editInfo.title;
+        note.text = editInfo.text;
+    } else {
+        note.id = model.notes[model.notes.length - 1].id + 1;
+        note.title = editInfo.title;
+        note.text = editInfo.text;
+        note.image = editInfo.image;
 
-note.title = editInfo.title;
-note.text = editInfo.text;
-} else {
-note.id = Math.floor(Math.random()*1000000 + 1);
-note.title = editInfo.title;
-note.text = editInfo.text;
+        model.notes.push(note);
+    }
 
-model.notes.push(note);
-}
+    const json = JSON.stringify(model);
+    fs.writeFileSync("./data/notes.json", json);
 
-const json = JSON.stringify(model);
-fs.writeFileSync("./data/notes.json", json);
-
-response.redirect("/");
+    response.redirect("/");
 });
 
 app.get("/admin/delete/:id", (request, response) => {
-let id = request.params.id;
+    let id = request.params.id;
 
-if (!id) response.redirect("/");
-id = Number(id);
+    if (!id) response.redirect("/");
+    id = Number(id);
 
-const file = fs.readFileSync("./data/notes.json");
-const model = JSON.parse(file);
-model.notes = model.notes.filter(x => x.id != id);
+    const file = fs.readFileSync("./data/notes.json");
+    const model = JSON.parse(file);
+    model.notes = model.notes.filter(x => x.id != id);
 
-const json = JSON.stringify(model);
-fs.writeFileSync("./data/notes.json", json);
+    const json = JSON.stringify(model);
+    fs.writeFileSync("./data/notes.json", json);
 
-response.redirect("/");
+    response.redirect("/");
 });
 
 app.get("/*", (request, response) => {
-response.redirect("/");
+    response.redirect("/");
 });
